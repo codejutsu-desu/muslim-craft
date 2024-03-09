b2493acf6b014e79451dfa6828c24e01fb7cf978
Code-Jutsu <mrimmoys@gmail.com>
2024-03-09 18:03:08 +0600

    acp

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -14,11 +14,11 @@ export default function HomePage() {
   return (
     <div>
       <div className="mb-8 space-y-4">
-        <h2 className="text-2xl md:text-4xl font-bold text-center">
-          Explore the power of AI
+        <h2 className="text-xl md:text-4xl font-bold text-center">
+          Let AI be your companion in your pursuit of ilm
         </h2>
         <p className="text-muted-foreground font-light text-sm md:text-lg text-center">
-          Chat with the smartest AI - Experience the power of AI
+          The Muslim AI- Never an alternative to your local scholar
         </p>
       </div>
       <div className="px-4 md:px-20 lg:px-32 space-y-4">
